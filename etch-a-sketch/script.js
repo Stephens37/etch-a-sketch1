cc48f9dc34ca88aabbfda6c954b8c050554f4019
Stephens37 <braydenstephens@protonmail.com>
2023-01-24 03:41:55 -0600

    Add userPrompt

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,11 +1,15 @@
+let userInput = prompt("Choose the number of squares.");
 let squareContainer = document.getElementById("grid-container");
-for (let i = 0; i < 256; i++){
+function createDivs(num){
+for (let num = 0; num < 100; num++){
     let squares = document.createElement("div");
     squares.classList.add("sketch");
   squares.addEventListener("mouseover", changeColorBlue);
     
   document.getElementById("grid-container").appendChild(squares);
 }
+}
+createDivs(userInput);
 
 function changeColorBlue(){
     this.style.backgroundColor = "blue";
