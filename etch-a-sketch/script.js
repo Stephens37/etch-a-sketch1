c9da97b701b6fdf4b6f2d921864a5113b7ded77e
Stephens37 <braydenstephens@protonmail.com>
2023-01-27 03:52:27 -0600

    Make squares darken over 10 times hovered on

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -7,8 +7,49 @@ for (let num = 0; num < (squareInput*squareInput); num++){
 
     squareInput.classList.add("sketch");
     squareInput.addEventListener("mouseover", () => {
+
         squareInput.style.backgroundColor = changeColor();
-    });
+        let brightness;
+            const currentBrightness = squareInput.style.filter;
+            
+            if(currentBrightness == ""){
+                brightness = "brightness(1)";
+            }
+            else {
+                if(currentBrightness === 'brightness(1)') {
+                    brightness = 'brightness(0.9)'
+                  } else if(currentBrightness === 'brightness(0.9)') {
+                    brightness = 'brightness(0.8)'
+                  }
+                  else if(currentBrightness === 'brightness(0.8)') {
+                    brightness = 'brightness(0.7)'
+                  }
+                  else if(currentBrightness === 'brightness(0.7)') {
+                    brightness = 'brightness(0.6)'
+                  }
+                  else if(currentBrightness === 'brightness(0.6)') {
+                    brightness = 'brightness(0.5)'
+                  }
+                  else if(currentBrightness === 'brightness(0.5)') {
+                    brightness = 'brightness(0.4)'
+                  }
+                  else if(currentBrightness === 'brightness(0.4)') {
+                    brightness = 'brightness(0.3)'
+                  }
+                  else if(currentBrightness === 'brightness(0.3)') {
+                    brightness = 'brightness(0.2)'
+                  }
+                  else if(currentBrightness === 'brightness(0.2)') {
+                    brightness = 'brightness(0.1)'
+                  }
+                  else if(currentBrightness === 'brightness(0.1)') {
+                    brightness = 'brightness(0)'
+                  }
+
+            }
+            squareInput.style.filter = brightness;
+        }
+    );
     
   document.getElementById("grid-container").appendChild(squareInput);
 }
@@ -28,4 +69,12 @@ function changeColor(){
     const g = randomBetween(0, 255);
     const b = randomBetween(0, 255);
     return `rgb(${r},${g}, ${b})`;
+}
+
+function modifyBrightness(val) {
+    let brightness = 1.0;
+    brightness = brightness + val;
+    brightness = parseInt(brightness, 10);
+    $("body_id").style.filter = "brightness(100)";
+    return brightness;
 }
